1c43483966032cd4e651ac0612b88c069db4c139
Agamyrat Chariyev <agamyrat.chariyev@gmail.com>
2020-08-08 19:45:24 +0500

    Test again

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php 
 	define('DB_TYPE','mysql');
 	define('DB_HOST','localhost');
-	define('DB_NAME','test_platform');
+	define('DB_NAME','tablename');
 	define('DB_USER','root');
-	define('DB_PASS','timarSistem4606');
+	define('DB_PASS','pass');
 	
  ?>
