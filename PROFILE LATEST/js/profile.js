3c7b83306b40309058a5fc84e30417cf04ac4fbe
Mahrijemal <mahrijemal.nepesova@gmail.com>
2020-08-16 18:51:53 +0500

    Latest Profile page

--- PROFILE LATEST/js/profile.js
+++ PROFILE LATEST/js/profile.js
@@ -1,9 +1,8 @@
 "use strict";
 
+// header hamburger
 let hamburger = document.querySelector(".hamburger-wrapper");
-
 let hamburgerMenu = document.querySelector(".header").querySelector(".right");
-
 let hamburgerMenuPos = hamburgerMenu.style.right;
 
 if (window.innerWidth < 548) {
@@ -13,11 +12,14 @@ if (window.innerWidth < 548) {
 hamburgerMenuPos = Number(hamburgerMenu.style.right.split("px", 1)[0]);
 
 hamburger.addEventListener("click", function (event) {
-  hamburger.classList.toggle("active");
-  hamburger.classList.toggle("inactive");
+  if (hamburgerMenuPos == -250 || hamburgerMenuPos == 0) {
+    hamburger.classList.toggle("active");
+    hamburger.classList.toggle("inactive");
+  }
 
-  if (hamburger.classList[1] == "active") {
+  if (hamburger.classList[1] == "active" && hamburgerMenuPos == -250) {
     let slideInInterval = setInterval(slideIn, 0.1);
+    console.log('hello1')
 
     hamburgerMenu.style.display = "flex";
 
@@ -31,8 +33,10 @@ hamburger.addEventListener("click", function (event) {
         hamburgerMenu.style.right = hamburgerMenuPos + "px";
       }
     }
-  } else {
+  } else if (hamburger.classList[1] == "inactive" && hamburgerMenuPos == 0) {
     let slideOutInterval = setInterval(slideOut, 0.1);
+
+    console.log('hello2')
 
     function slideOut() {
       if (hamburgerMenuPos <= -250) {
@@ -47,3 +51,32 @@ hamburger.addEventListener("click", function (event) {
     }
   }
 });
+
+
+// header dropdowns
+
+// language dropdown
+let languageBlock = document.querySelector('.header-language')
+let languageDropdown = document.querySelector('.dropdown-language')
+
+languageBlock.addEventListener('mouseover', function () {
+  languageDropdown.style.display = 'block'
+})
+
+languageBlock.addEventListener('mouseleave', function () {
+  languageDropdown.style.display = 'none'
+})
+
+// profile dropdown
+let profileBlock = document.querySelector('.header-avatar')
+let profileDropdown = document.querySelector('.dropdown-avatar')
+
+profileBlock.addEventListener('mouseover', function () {
+  profileDropdown.style.display = 'block'
+})
+
+profileBlock.addEventListener('mouseleave', function () {
+  profileDropdown.style.display = 'none'
+})
+
+
