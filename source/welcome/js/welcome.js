b23745d0777b84c27b7de61860683d61f1f12c76
Erkin Rejepow <erejepow00@mail.ru>
2020-08-30 16:32:42 +0500

    some changes

--- source/welcome/js/welcome.js
+++ source/welcome/js/welcome.js
@@ -61,48 +61,30 @@ document.querySelector('.dropdown').addEventListener('mouseout', function () {
 
 $('form').on('submit',function(e){
    e.preventDefault()
-   var formData = new FormData(this);
-   $.ajax({
-      url:'mailnotification/contactUs',
-      type:'post',
-      processData: false,
-      contentType: false,
-      data: formData,
-      success:function(data){
-         if(data !=0){
-            alert('Thanks for contacting us!');
-         }else{
-            alert("Your message couldn't be sent!");
-         }
-      },
-      error:function(){
-         console('error boldy',data);
-      }
-   });
-});
-/* document.querySelector('form').onsubmit = (e) => {
-   e.preventDefault()
-   var formData = new FormData(this);
-   alert(1);
-   $.ajax({
-      url:'mailnotification/contactUs',
-      type:'post',
-      processData: false,
-      contentType: false,
-      data: formData,
-      success:function(data){
-         if(data !=0){
-            alert('Thanks for contacting us!');
-         }else{
-            alert("Your message couldn't be sent!");
-         }
-      },
-      error:function(){
-         console('error boldy',data);
-      }
-   });
-} */
+   $("#name").val(null);
+   $("#email").val(null);
+   $("#message").val(null);
 
+      var formData = new FormData(this);
+      $.ajax({
+         url:'mailnotification/contactUs',
+         type:'post',
+         processData: false,
+         contentType: false,
+         data: formData,
+         success:function(data){
+            if(data !=0){
+               alert('Thanks for contacting us!');
+            }else{
+               alert("Your message couldn't be sent!");
+            }
+         },
+         error:function(){
+            console('error boldy',data);
+         }
+      });
+  
+});
 
 //Validation Inputs
 
